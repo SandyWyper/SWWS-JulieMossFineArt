1ffb0e069f81719b3cfd2e3eb1d070bffff73fe3
sandywyper <sandywyper@gmail.com>
2020-09-02 11:03:46 +0100

    Fix favicon pathname

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#FFFFFF`,
         display: `minimal-ui`,
-        icon: `./assets/favicon-32x32.png`, // This path is relative to the root of the site.
+        icon: `./static/uploads/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     {
