ebbf464c83a23745d008fd1623ac7fc7c5029a8e
sandywyper <sandywyper@gmail.com>
2020-09-04 10:08:49 +0100

    Speed test without fade page transitions

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,6 @@ require('./src/styles/purgedStyles.scss');
 require('./src/styles/unpurgedStyles.scss');
 
 // turning off the default behaviour which will now be handled by 'gatsby-plugin-transitions'
-exports.shouldUpdateScroll = () => {
-  return false;
-};
+// exports.shouldUpdateScroll = () => {
+//   return false;
+// };
