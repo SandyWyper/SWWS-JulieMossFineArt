b9e52197bc5fc4099c0f4f6a16d0b2fe8ccc6225
sandywyper <sandywyper@gmail.com>
2020-09-21 16:44:17 +0100

    New 404 page illistration added

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Link } from 'gatsby';
 import SEO from '../components/seo';
 import Footer from '../components/footer';
+import Robot404 from '../components/Robot404';
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
     <div className="relative min-h-screen footer-padding">
-      <section className="max-w-5xl px-4 pt-24 mx-auto ">
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        <Link to="/">Go home ...</Link>
+      <section className="max-w-5xl px-4 pt-24 mx-auto md:flex">
+        <div className="md:w-1/2">
+          <h1 className="mb-0 opacity-50">NOT FOUND</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <Link to="/" className="hidden md:block btn">
+            home
+          </Link>
+        </div>
+        <div className="md:w-1/2">
+          <Robot404 classes="max-w-lg" />
+          <Link to="/" className="md:hidden btn">
+            home
+          </Link>
+        </div>
       </section>
     </div>
     <Footer />
