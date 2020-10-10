1f5c0f79034cafcf1daa7fe648c32155ecbc0712
sandywyper <sandywyper@gmail.com>
2020-10-10 16:02:29 +0100

    FIX stop builds from failing when artwork has no image.

--- src/templates/artworkTemplate.js
+++ src/templates/artworkTemplate.js
@@ -9,10 +9,8 @@ import Fade from 'react-reveal/Fade';
 const ArtworkTemplate = (props) => {
   const { frontmatter, html } = props.data.markdownRemark;
   const { next, prev } = props.pageContext;
-  const sharingImageAlt = props.data.markdownRemark.frontmatter.images[0].alt;
-  const sharingImage = props.data.markdownRemark.frontmatter.images[0].image
-    ? props.data.markdownRemark.frontmatter.images[0].image.childImageSharp.resize.src
-    : null;
+  const sharingImageAlt = frontmatter.images !== null ? frontmatter.images[0].alt : null;
+  const sharingImage = frontmatter.images !== null ? frontmatter.images[0].image.childImageSharp.resize.src : null;
 
   const ArtWorkInfo = () => (
     <div className="max-w-xl mx-auto">
@@ -27,25 +25,26 @@ const ArtworkTemplate = (props) => {
       <div className="relative min-h-screen footer-padding">
         <section className="max-w-5xl px-4 pt-24 mx-auto text-left artwork-grid">
           <div className="pb-12 artwork-space md:pl-4">
-            {frontmatter.images.map((art, i) => {
-              if (art.image !== null) {
-                return (
-                  <Fade key={art.alt + i} duration={1500}>
-                    <Img
-                      className="max-w-xl mx-auto mb-4"
-                      fluid={{
-                        ...art.image.childImageSharp.fluid,
-                        sizes: '(max-width: 640px) calc(100vw - 2rem), 750px',
-                      }}
-                      alt={art.alt}
-                      loading={i === 0 ? 'eager' : 'lazy'}
-                    />
-                  </Fade>
-                );
-              } else {
-                return null;
-              }
-            })}
+            {frontmatter.images !== null &&
+              frontmatter.images.map((art, i) => {
+                if (art.image !== null) {
+                  return (
+                    <Fade key={art.alt + i} duration={1500}>
+                      <Img
+                        className="max-w-xl mx-auto mb-4"
+                        fluid={{
+                          ...art.image.childImageSharp.fluid,
+                          sizes: '(max-width: 640px) calc(100vw - 2rem), 750px',
+                        }}
+                        alt={art.alt}
+                        loading={i === 0 ? 'eager' : 'lazy'}
+                      />
+                    </Fade>
+                  );
+                } else {
+                  return null;
+                }
+              })}
             <ArtWorkInfo />
             <div className="w-full max-w-xl mx-auto font-normal">
               {prev && (
